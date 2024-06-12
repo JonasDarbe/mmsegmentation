7da64c586c44ea2cee8d1b90140c3e763b865250
Jonas Stankevičius <jonas_stankevicius@trimble.com>
2024-06-12 14:58:51 +0300

    add trdm dataset

---
+++ mmseg/datasets/trdm.py
@@ -0,0 +1,17 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class TRDMDataset(BaseSegDataset):
+    METAINFO = dict(
+        classes=('road',),
+        palette=[[0, 192, 64]])
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='.png',
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
